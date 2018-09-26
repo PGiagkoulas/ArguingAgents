f0162e9b23e1a3845d119920b46dbb279706bb94
Sohyo <cienakim12@gmail.com>
2018-09-26 16:42:52 +0200

    created new arg1

--- JurySimulation/src/Main.java
+++ JurySimulation/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Court simulation = new Court();
+		Argument arg1 = new Argument();
 		System.out.println(simulation.toString());
 	}
 
